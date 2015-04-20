85463ffcb1654b4d5fbd7db3f317cb83b1054c1e
Mahesh <s.maheshbabu@gmail.com>
2015-04-19 22:43:55 -0700

    Workaround for https://github.com/angular-ui/bootstrap/issues/2659

--- app.js
+++ app.js
@@ -367,5 +367,19 @@ app.controller('ClockController', ['$scope', '$interval', 'TimeZoneClocksManager
 	};
 }]);
 
+// Temporary workaround for https://github.com/angular-ui/bootstrap/issues/2659 to avoid showing the
+// entire date in the datepicker text box. Once the above issue is fixed, make sure the latest bootstrap-ui
+// is being used and remove the following directive.
+app.directive('datepickerPopup', function (){
+    return {
+        restrict: 'EAC',
+        require: 'ngModel',
+        link: function(scope, element, attr, controller) {
+			//remove the default formatter from the input directive to prevent conflict
+			controller.$formatters.shift();
+		}
+	}
+});
+
 })();
 
