d3d69e6b7a9fc7bfdc02c6e1b088d0b9f380956c
Mahesh <s.maheshbabu@gmail.com>
2015-04-20 01:05:43 -0700

    Avoid duplication in the addTimeZone logic

--- app.js
+++ app.js
@@ -75,52 +75,29 @@ app.service('TimeZoneClocksManager', ['TimeZoneObject', 'OffsetBasedTimeZoneObje
 		// The title can be any value but is usually used to contain a user friendly name to identify the time zone
 		// like a city name, country name etc. If missing, the time zone name will be used as title.
 		addTimeZone: function(timeZoneToBeAdded, titleOfTheTimeZone) {
-			var addedTimeZones = this.addedTimeZones();
-			var localTimeZoneObject = this.localTimeZoneObject();
-			console.log("We already have " + addedTimeZones.length + " timeZones and we are adding " + timeZoneToBeAdded + " under the title " + titleOfTheTimeZone);
-
-			for (var i = 0; i < addedTimeZones.length; i++) {
-				if(addedTimeZones[i].title === titleOfTheTimeZone) {
-					console.log("Requested time zone already exists. Moving it to the top");
-					var timeZoneObjectsToBeBubbledUp = addedTimeZones.splice(i, 1);
-					addedTimeZones = timeZoneObjectsToBeBubbledUp.concat(addedTimeZones);
-					allTimeZones = [allTimeZones[0]].concat(addedTimeZones);
-					return;
-				}
-			}
+			console.log("We already have " + this.addedTimeZones().length + " timeZones and we are adding " + timeZoneToBeAdded + " under the title " + titleOfTheTimeZone);
 
 			var timeZoneObjectToBeAdded = new TimeZoneObject(timeZoneToBeAdded, titleOfTheTimeZone);
-			timeZoneObjectToBeAdded.timerManager(clocksRunning);
-
-			// If clocks are not running, new timeZones being added shouldn't show the current time in that timeZone.
-			// We should instead pick any of the valid existing clocks, convert the time to the new timeZone and show it.
-			// If all the clocks are invalid, we just set the new clock to its current time, in a stopped state.
-			if(!clocksRunning) {
-				if(allTimeZones.length > 0) {
-					var aValidTimeZoneObject;
-					for (var i = 0; i < allTimeZones.length; i++) {
-						if(!allTimeZones[i].invalidTime) {
-							aValidTimeZoneObject = allTimeZones[i];
-							break;
-						}
-					}
-
-					if(aValidTimeZoneObject) {
-						timeZoneObjectToBeAdded.setMomentFromUTC(aValidTimeZoneObject.getMomentInUTC());
-					}
-				}
-			}
-			addedTimeZones.push(timeZoneObjectToBeAdded);
-			allTimeZones = [allTimeZones[0]].concat(addedTimeZones);
+			this._addTimeZone(timeZoneObjectToBeAdded, titleOfTheTimeZone);
 		},
 		// Adds a new timeZone object that represents time at the given offset. If an object for the given
 		// offset already exists, we just move the corresponding object to the top of the list.
 		// The title can be any value but is usually used to contain a user friendly name to identify the time zone
 		// like GMT+5:30, UTC-1 etc. If missing, the time zone name will be used as title.
 		addOffsetBasedTimeZone: function(offsetInMinutes, titleOfTheTimeZone) {
+			console.log("We already have " + this.addedTimeZones().length + " timeZones and we are adding time zone at offset " + offsetInMinutes + " under the title " + titleOfTheTimeZone);
+			
+			var timeZoneObjectToBeAdded = new OffsetBasedTimeZoneObject(offsetInMinutes, titleOfTheTimeZone);
+			this._addTimeZone(timeZoneObjectToBeAdded, titleOfTheTimeZone);
+		},
+		stopClocks: function() {
+			clocksRunning = false;
+			for (var i = 0; i < allTimeZones.length; i++) {
+				allTimeZones[i].timerManager(clocksRunning);
+			}
+		},
+		_addTimeZone: function(timeZoneObjectToBeAdded, titleOfTheTimeZone) {
 			var addedTimeZones = this.addedTimeZones();
-			var localTimeZoneObject = this.localTimeZoneObject();
-			console.log("We already have " + addedTimeZones.length + " timeZones and we are adding time zone at offset " + offsetInMinutes + " under the title " + titleOfTheTimeZone);
 
 			for (var i = 0; i < addedTimeZones.length; i++) {
 				if(addedTimeZones[i].title === titleOfTheTimeZone) {
@@ -132,7 +109,6 @@ app.service('TimeZoneClocksManager', ['TimeZoneObject', 'OffsetBasedTimeZoneObje
 				}
 			}
 
-			var timeZoneObjectToBeAdded = new OffsetBasedTimeZoneObject(offsetInMinutes, titleOfTheTimeZone);
 			timeZoneObjectToBeAdded.timerManager(clocksRunning);
 
 			// If clocks are not running, new timeZones being added shouldn't show the current time in that timeZone.
@@ -155,12 +131,6 @@ app.service('TimeZoneClocksManager', ['TimeZoneObject', 'OffsetBasedTimeZoneObje
 			}
 			addedTimeZones.push(timeZoneObjectToBeAdded);
 			allTimeZones = [allTimeZones[0]].concat(addedTimeZones);
-		},
-		stopClocks: function() {
-			clocksRunning = false;
-			for (var i = 0; i < allTimeZones.length; i++) {
-				allTimeZones[i].timerManager(clocksRunning);
-			}
 		}
     }
 }]);
